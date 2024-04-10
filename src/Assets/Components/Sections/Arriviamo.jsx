bb34baed9cf3a645a5909117e64ba1f9da9ee8bb
davide122 <davidemarchica277@gmail.com>
2024-04-10 18:51:04 +0200

    coming

--- src/Assets/Components/Sections/Arriviamo.jsx
+++ src/Assets/Components/Sections/Arriviamo.jsx
@@ -1,7 +1,21 @@
+import { HiOutlinePaperAirplane } from "react-icons/hi2";
+import logo from "../Commons/Image/logo.png"
 const Arriviamo = () => {
     return(
-<div className="vh-100 d-flex justify-content-center align-items-center text-center">
+<div className="vh-100 d-flex justify-content-center align-items-center text-center flex-column">
+    <img src={logo} alt=""className="logo" />
 <h1 className="Title">Stiamo arrivando!</h1>
+
+<form name="contact" className="contact-form" netlify>
+    <div className="d-flex">
+  <p className="">
+    <label> <input type="email" placeholder="do@gmail.com" name="email" className="" /></label>
+  </p>
+  <p>
+    <button type="submit"><HiOutlinePaperAirplane></HiOutlinePaperAirplane></button>
+  </p>
+    </div>
+</form>
 </div>
     )
 }
