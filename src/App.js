3c7342a68bc10e0145d53bd987c88a96bf4ff643
davide122 <davidemarchica277@gmail.com>
2024-04-10 12:21:42 +0200

    sistemazione

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './Assets/Views/HomePage';
 
-
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Color from "./Assets/Components/Commons/Style/Utils/Color.css";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
